013b391034cdc6135c2d9018e3517f1bac1ed374
Luke Eldridge <jleldridge27@gmail.com>
2021-01-01 14:57:25 -0800

    Add projects route.

--- website/app/router.js
+++ website/app/router.js
@@ -1,12 +1,13 @@
-import EmberRouter from '@ember/routing/router';
-import config from 'website/config/environment';
+import EmberRouter from "@ember/routing/router";
+import config from "website/config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  this.route('home');
-  this.route('blog');
+Router.map(function () {
+  this.route("home");
+  this.route("projects");
+  this.route("blog");
 });
