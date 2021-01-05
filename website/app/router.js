6fffe518fd8cd61c18c50a20192670657fa5bfa2
Luke Eldridge <jleldridge27@gmail.com>
2021-01-04 22:02:56 -0800

    Make home the index route.

--- website/app/router.js
+++ website/app/router.js
@@ -7,7 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route("home");
+  this.route("index", { path: "/" });
   this.route("projects");
   this.route("blog");
 });
