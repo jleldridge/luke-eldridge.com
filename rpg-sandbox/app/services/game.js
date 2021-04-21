40c4507f504e9c52f7e3bc032041421483aaef6f
Luke Eldridge <jleldridge27@gmail.com>
2021-04-20 21:34:13 -0700

    Add demo animation to game loop.

--- rpg-sandbox/app/services/game.js
+++ rpg-sandbox/app/services/game.js
@@ -1,6 +1,8 @@
 import Service from '@ember/service';
 
 export default class GameService extends Service {
+  entities = [];
+
   gameLoop() {
     this.animReq = window.requestAnimationFrame(this.gameLoop.bind(this));
     this.update();
@@ -8,14 +10,34 @@ export default class GameService extends Service {
   }
 
   start() {
+    this.entities = [{x: 0, y: 0, dx: 1, dy: 1}];
     this.gameLoop();
   }
 
+  pause() {
+    window.cancelAnimationFrame(this.animReq);
+  }
+
   update() {
-    // console.log("updating");
+    if (!this.canvas) this.canvas = window.document.getElementById('game-canvas');
+    if (!this.canvas) return;
+
+    for (let e of this.entities) {
+      e.x += e.dx;
+      e.y += e.dy;
+
+      if (e.x + 20 >= 800 || e.x <= 0) e.dx *= -1;
+      if (e.y + 20 >= 600 || e.y <= 0) e.dy *= -1;
+    }
   }
 
   draw() {
-    // console.log("drawing");
+    if (!this.canvas) return;
+    let ctx = this.canvas.getContext('2d');
+    for (let e of this.entities) {
+      ctx.clearRect(0, 0, 800, 600)
+      ctx.fillStyle = 'white';
+      ctx.fillRect(e.x, e.y, 20, 20);
+    }
   }
 }
