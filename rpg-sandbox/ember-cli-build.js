a2b70e57ee64364e2389f115da11126409ac24fa
Luke Eldridge <jleldridge27@gmail.com>
2021-04-21 21:51:27 -0700

    Disable SRI

--- rpg-sandbox/ember-cli-build.js
+++ rpg-sandbox/ember-cli-build.js
@@ -4,9 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    SRI: {
+      enabled: false
+    }
   });
-  SRI.paranoidCheck = false
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
