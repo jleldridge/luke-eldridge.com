00c7b3a334f9ea66e16aee038769046f8350788f
Luke Eldridge <jleldridge27@gmail.com>
2021-04-21 21:42:28 -0700

    Try to fix integrity issues.

--- rpg-sandbox/ember-cli-build.js
+++ rpg-sandbox/ember-cli-build.js
@@ -6,6 +6,7 @@ module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
+  SRI.paranoidCheck = false
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
