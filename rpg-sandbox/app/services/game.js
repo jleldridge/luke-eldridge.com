43efa914552b00f4ad68cc573e4b5543a6e9afba
Luke Eldridge <jleldridge27@gmail.com>
2021-04-21 23:50:58 -0700

    Add keyboard input.

--- rpg-sandbox/app/services/game.js
+++ rpg-sandbox/app/services/game.js
@@ -1,9 +1,13 @@
-import Service from '@ember/service';
+import Service, { inject as service } from '@ember/service';
 
 export default class GameService extends Service {
+  @service keyboardInput;
+
   entities = [];
+  player = {x: 0, y: 0, dx: 0, dy: 0};
 
   initGame() {
+    this.keyboardInput.attach();
     this.entities = [{x: 0, y: 0, dx: 1, dy: 1}];
   }
 
@@ -32,15 +36,31 @@ export default class GameService extends Service {
       if (e.x + 20 >= 800 || e.x <= 0) e.dx *= -1;
       if (e.y + 20 >= 600 || e.y <= 0) e.dy *= -1;
     }
+
+    this.player.dx = 0;
+    this.player.dy = 0;
+
+    this.player.dx += this.keyboardInput.keys['a'] ? -3 : 0;
+    this.player.dx += this.keyboardInput.keys['d'] ? 3 : 0;
+    this.player.dy += this.keyboardInput.keys['w'] ? -3 : 0;
+    this.player.dy += this.keyboardInput.keys['s'] ? 3 : 0;
+
+    this.player.x += this.player.dx;
+    this.player.y += this.player.dy;
   }
 
   draw() {
     if (!this.canvas) return;
+
     let ctx = this.canvas.getContext('2d');
+    ctx.clearRect(0, 0, 800, 600)
+
     for (let e of this.entities) {
-      ctx.clearRect(0, 0, 800, 600)
       ctx.fillStyle = 'white';
       ctx.fillRect(e.x, e.y, 20, 20);
     }
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(this.player.x, this.player.y, 20, 20);
   }
 }
---
+++ rpg-sandbox/app/services/keyboard-input.js
@@ -0,0 +1,29 @@
+import Service from '@ember/service';
+
+export default class KeyboardInputService extends Service {
+  keys = {
+    'w': false,
+    'a': false,
+    's': false,
+    'd': false
+  }
+
+  attach() {
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
+    window.addEventListener('keyup', this.onKeyUp.bind(this));
+
+  }
+
+  detach() {
+    window.removeEventListener('keydown', this.onKeyDown.bind(this));
+    window.removeEventListener('keyup', this.onKeyUp.bind(this));
+  }
+
+  onKeyDown(event) {
+    this.keys[event.key] = true;
+  }
+
+  onKeyUp(event) {
+    this.keys[event.key] = false;
+  }
+}
