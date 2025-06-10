31cd8e38fe2ba7c52b5c465c9680dbdd6be9194a
Luke Eldridge <jleldridge27@gmail.com>
2025-06-10 00:22:51 -0700

    Allow animating arbitrary tags from content div.

--- web/index.js
+++ web/index.js
@@ -1,20 +1,50 @@
-header = document.getElementById('header');
-desiredText = document.getElementById('hidden-header').textContent;
-let headerContainer = header.getElementsByTagName('h1')[0];
+const animatedContent = document.getElementById('animated-content');
+const animatedContainer = document.getElementById('animated-container')
+const tags = animatedContent.children;
+const typeDelay = 100; // milliseconds between new letters appearing
 
-lastTimestamp = null;
-typeDelay = 100; // milliseconds between new letters appearing
-currentLetterIndex = 0
+let lastTimestamp = null;
+let currentLetterIndex = 0
+let tagIndex = 0;
+let currentTag = null;
+let tagTextContent = null;
+let currentContainer = null;
 
 function animateText(timestamp) {
-  let done = currentLetterIndex > desiredText.length - 1;
+  let done = tagIndex >= tags.length;
   if (done) return;
+  
+  // get the next tag
+  if (currentTag == null) {
+    currentTag = tags[tagIndex];
+    tagTextContent = currentTag.textContent;
+    currentLetterIndex = 0;
+    currentContainer = document.createElement(currentTag.tagName);
+    let attributes = currentTag.attributes;
+    for (const kvp of attributes) {
+      console.log(kvp.nodeName, kvp.nodeValue);
+      currentContainer.setAttribute(kvp.nodeName, kvp.nodeValue);
+    }
+    animatedContainer.appendChild(currentContainer);
+  }
+
+  let endOfLine = currentLetterIndex > tagTextContent.length - 1;
+  if (endOfLine) {
+    // remove "cursor" from end of line
+    currentContainer.textContent = tagTextContent;
+
+    // clear currentTag for next line
+    currentTag = null;
+    tagIndex += 1;
+    requestAnimationFrame(animateText);
+    return;
+  }
 
   if (lastTimestamp == null) lastTimestamp = timestamp;
   let delta = timestamp - lastTimestamp;
 
   if (delta >= typeDelay) {
-    headerContainer.textContent = desiredText.substring(0, currentLetterIndex + 1) + '\u2588';
+    currentContainer.textContent = tagTextContent.substring(0, currentLetterIndex + 1) + '\u2588';
     currentLetterIndex++;
     lastTimestamp = timestamp;
   }
